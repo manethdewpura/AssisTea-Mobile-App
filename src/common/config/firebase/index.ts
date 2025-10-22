5ed59f023bf16addad791ea56cdc13ac88418913
Maneth Dewpura <dewpuramaneth@gmail.com>
2025-10-22 16:48:37 +0530

    Update project configuration and dependencies
    - Added Firebase configuration and initialized Firebase app in the new src/common/config/firebase/index.ts file.
    - Introduced google-services.json for Firebase integration in the Android app.
    - Updated Android build.gradle files to include Google services plugin.
    - Enhanced .gitignore to exclude stack dump files.
    - Updated package.json and package-lock.json to include new dependencies for Firebase and Redux.

---
+++ src/common/config/firebase/index.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from '@react-native-firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVpKyJAV-57VUVmOjvKLTwSSb2K0rZRjc",
+  authDomain: "assistea.firebaseapp.com",
+  projectId: "assistea",
+  storageBucket: "assistea.appspot.com",
+  messagingSenderId: "313438996875",
+  appId: "1:313438996875:android:abd29ddb01feaa29a8878e",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
+
