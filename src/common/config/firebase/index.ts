261671821ba13ae1c4581fbb466f8cb9c312825c
Maneth Dewpura <dewpuramaneth@gmail.com>
2025-10-22 17:41:23 +0530

    Add environment variable support and update configuration
    - Added support for environment variables using react-native-dotenv.
    - Updated .gitignore to include .env files for sensitive information.
    - Modified babel.config.js to include dotenv plugin for environment variable management.
    - Updated README.md with instructions for setting up environment variables.
    - Refactored Firebase configuration to use environment variables for sensitive data.

--- src/common/config/firebase/index.ts
+++ src/common/config/firebase/index.ts
@@ -1,12 +1,20 @@
 import { initializeApp } from '@react-native-firebase/app';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVpKyJAV-57VUVmOjvKLTwSSb2K0rZRjc",
-  authDomain: "assistea.firebaseapp.com",
-  projectId: "assistea",
-  storageBucket: "assistea.appspot.com",
-  messagingSenderId: "313438996875",
-  appId: "1:313438996875:android:abd29ddb01feaa29a8878e",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
