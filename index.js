d21ca725cd7fd0441fef91dc10245ecb67964c28
manethdewpura <dewpuramaneth@gmail.com>
2026-02-06 01:24:44 +0530

    chore: Integrate LogBox to manage warning messages in index.js

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+// Mute RN yellow box warnings in emulator
+// LogBox.ignoreAllLogs(true);
 
 AppRegistry.registerComponent(appName, () => App);
